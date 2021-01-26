67aca1efd9ca71f43789c800fc21b47a5b13d49c
Youssef Mdb <aiesec.youssefmeddeb@gmail.com>
2021-01-26 20:17:48 +0100

    add values

--- index.php
+++ index.php
@@ -1,32 +1,48 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP Store</title>
-  </head>
-  <body>
-    <?php
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
-    $products['Computer']=750;
-    $products['Car']=15000;
-     $products['iPhone']=1000;
-     $products['Toaster']=75;
-    $name = "PHP Store";
-    $credit = 1000;
-    echo "<h1>Welcome to ".$name."!</h1>";
-    echo "<h2>You have $".$credit." in your wallet.</h2>";
-    
-    foreach($products as $key => $value){
-	    echo "<p>The ".$key." costs ".$value."</p>";
-    }
 
-    echo "<h2>Items you can afford</h2>";
+<head>
+  <title>PHP Store</title>
+</head>
 
-foreach($products as $key => $value){
-  if($value <= $credit ){
-  	echo "<p>".$key."</p>"; 
+<body>
+  <?php
+  // Using two forward slashes we create comments in php.
+  // Within the php tag, we can insert values into our HTML.
+  $products['Computer'] = 750;
+  $products['Car'] = 15000;
+  $products['iPhone'] = 1000;
+  $products['Toaster'] = 75;
+  $name = "PHP Store";
+  $credit = 1000;
+  echo "<h1>Welcome to " . $name . "!</h1>";
+  echo "<h2>You have $" . $credit . " in your wallet.</h2>";
+
+  foreach ($products as $key => $value) {
+    echo "<p>The " . $key . " costs " . $value . "</p>";
   }
-}
-    ?>
-  </body>
+
+  echo "<h2>Items you can afford</h2>";
+
+  foreach ($products as $key => $value) {
+    if ($value <= $credit) {
+      echo "<p>" . $key . "</p>";
+    }
+  }
+  $amount = 800;
+  $taxRate = 0.0825;
+  $addedTax = $amount * $taxRate;  //amount = 800, tax = .0825
+  echo $addedTax."</br>";
+
+  function tax_calc($amount, $taxRate)
+  {
+    $calculate_tax = $amount * $taxRate;
+    $amount = round($amount + $calculate_tax, 2);
+    return $amount;
+  }
+  echo (tax_calc($amount,$taxRate));
+  ?>
+  
+</body>
+
 </html>
