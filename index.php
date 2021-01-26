5dd23405704592eded25b35213c06c9957ebada3
Youssef Mdb <aiesec.youssefmeddeb@gmail.com>
2021-01-26 20:00:12 +0100

    add values

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
     <?php
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
+    $products['Computer']=750;
+    $products['Car']=15000;
+     $products['iPhone']=1000;
+     $products['Toaster']=75;
     $name = "PHP Store";
     $credit = 1000;
     echo "<h1>Welcome to ".$name."!</h1>";
     echo "<h2>You have $".$credit." in your wallet.</h2>";
+    echo "<p>A car costs ".$products['Car']."</p>";
     ?>
   </body>
 </html>
